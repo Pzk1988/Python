9f7ace30d41cdb43477ab01a4c6ae1aeae7d4645
przemyslaw_knapik <knapik.przemyslaw@gmail.com>
2016-01-10 19:18:38 +0100

    a

--- gauss method.py
+++ gauss method.py
@@ -8,21 +8,44 @@ class LinearEquasions:
         for i in range(self.iSize):
             for j in range(self.iSize + 1):
                 print(i, ", ", j, ", ", (i*(self.iSize + 1)) + j) 
-                self.matrix[(i*(self.iSize + 1)) + j] = int(input("Enter "))
+                self.matrix[(i*(self.iSize + 1)) + j] = float(input("Enter "))
 
     def printMatrix(self):
-        print(self.iSize)
         for i in range(self.iSize):
-            print("|", end="")
+            print("| ", end="")
             for j in range(self.iSize + 1):
                 if j < self.iSize - 1:
                     print(self.matrix[(i*(self.iSize + 1)) + j], " ", end="")
                 else:
-                    print(self.matrix[(i*(self.iSize + 1)) + j], "|", end="")
+                    print(self.matrix[(i*(self.iSize + 1)) + j], "| ", end="")
             print("")
 
+    def gauss(self):
+        for k in range(self.iSize): #iteracja
+            print("k = ", k)
+            firstInIteration = self.matrix[(self.iSize + 1) * k]
+            for i in range(k, self.iSize): #wiersze
+                firstInRow = self.matrix[(self.iSize + 1) * i + k]
+                print("i = ", i, ", ", firstInRow)
+                for j in range(k, self.iSize + 1): #kolumny
+                    if k == i:
+                        print("tu ", self.matrix[(self.iSize + 1) * i + j], "=", self.matrix[(self.iSize + 1) * i + j], "/", firstInRow)
+                        self.matrix[(self.iSize + 1) * i + j] = self.matrix[(self.iSize + 1) * i + j] / firstInRow
+                        #print(self.matrix[(self.iSize + 1) * i + j], self.matrix[(self.iSize + 1) * i])
+                    else:
+                        print("jestem ", self.matrix[(self.iSize + 1) * i + j], "=", self.matrix[(self.iSize + 1) * i + j], "-", firstInRow, "*", self.matrix[(self.iSize + 1) * (i - 1) + j])
+                        print((self.iSize + 1) * i + j, "self.iSize", "+", "1", "*", "i", "+", "j")
+                        self.matrix[(self.iSize + 1) * i + j] = self.matrix[(self.iSize + 1) * i + j] - firstInRow * self.matrix[(self.iSize + 1) * k + j]
+
+                self.printMatrix()
+                print("")
+                    
+
+        
 linearEquasion = LinearEquasions()
 
 linearEquasion.getSizeOf()
 linearEquasion.createMatrix()
 linearEquasion.printMatrix()
+print("GAUSS")
+linearEquasion.gauss()
